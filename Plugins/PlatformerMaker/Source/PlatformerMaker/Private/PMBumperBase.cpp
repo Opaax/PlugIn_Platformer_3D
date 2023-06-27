7f9e67898b945ac72884fcaccb414d3ea9a971de
Enguerran <61538252+Opaax@users.noreply.github.com>
2023-06-27 18:05:12 +0200

    [Add] base bumper [add] base bumper blueprint

--- Plugins/PlatformerMaker/Source/PlatformerMaker/Private/PMBumperBase.cpp
+++ Plugins/PlatformerMaker/Source/PlatformerMaker/Private/PMBumperBase.cpp
@@ -1,27 +1,87 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Enguerran COBERT, All Rights Reserved.
 
 
 #include "PMBumperBase.h"
 
-// Sets default values
+//Unreal
+#include "Components/BoxComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Character.h"
+#include "Components/PrimitiveComponent.h"
+
 APMBumperBase::APMBumperBase()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	m_root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(m_root);
+
+	m_boxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	m_boxComponent->SetupAttachment(m_root);
+
+	m_baseBumper = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseBumper"));
+	m_baseBumper->SetupAttachment(m_root);
+
+	m_topBumper = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TopBumper"));
+	m_topBumper->SetupAttachment(m_baseBumper);
 }
 
-// Called when the game starts or when spawned
 void APMBumperBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	BindBoxComponentEvent();
 }
 
-// Called every frame
 void APMBumperBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
 
+void APMBumperBase::BindBoxComponentEvent()
+{
+	if (m_boxComponent)
+	{
+		m_boxComponent->OnComponentBeginOverlap.AddDynamic(this, &APMBumperBase::OnBoxComponentOverlapped);
+	}
+	else
+	{
+		UE_LOG(LogPlatformerPlugin, Error, TEXT("%s, box component is not constructed"), *GetName());
+	}
+}
+
+void APMBumperBase::OnBoxComponentOverlapped(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (IsValid(OtherActor) && m_validClassToBump.Contains(OtherActor->GetClass()))
+	{
+		UE_LOG(LogPlatformerPlugin, Warning, TEXT("Valid Bump"));
+
+		if (ACharacter* lCharac = Cast<ACharacter>(OtherActor))
+		{
+			FVector lForceVector = OtherActor->GetActorUpVector() * m_bumperForce;
+
+			lCharac->LaunchCharacter(lForceVector, false, true);
+
+			return;
+		}
+
+		if (UPrimitiveComponent* lPrimitive = Cast<UPrimitiveComponent>(OtherActor->GetRootComponent()))
+		{
+			FVector lForceVector = OtherActor->GetActorUpVector() * (m_bumperForce * m_bumperActorForceFactor);
+
+			UE_LOG(LogPlatformerPlugin, Warning, TEXT("%f"), lPrimitive->GetPhysicsLinearVelocity().Z);
+
+			if (lPrimitive->GetPhysicsLinearVelocity().Z != 0)
+			{
+				lPrimitive->SetPhysicsLinearVelocity(FVector::Zero());
+			}
+
+			UE_LOG(LogPlatformerPlugin, Warning, TEXT("%f"), lPrimitive->GetPhysicsLinearVelocity().Z);
+
+			lPrimitive->AddImpulse(lForceVector);
+		}
+	}
 }
 
--- Plugins/PlatformerMaker/Source/PlatformerMaker/Public/PMBumperBase.h
+++ Plugins/PlatformerMaker/Source/PlatformerMaker/Public/PMBumperBase.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Enguerran COBERT, All Rights Reserved.
 
 #pragma once
 
@@ -6,21 +6,74 @@
 #include "GameFramework/Actor.h"
 #include "PMBumperBase.generated.h"
 
+class UBoxComponent;
+class USceneComponent;
+class UStaticMeshComponent;
+
 UCLASS()
 class PLATFORMERMAKER_API APMBumperBase : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+	/**************************** MEMBERS ******************************/
+protected:
+
+	/**************************** Settings ******************************/
+
+	/*
+	* base force that bumper while apply on character and actors
+	* if the overlapped actor is a character, consider that the bumper while launch character, otherwise will impulse on the root
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Setting", meta = (DisplayName = "BumperForce"))
+	float m_bumperForce = 2000.f;
+
+	/*
+	* Basic Actor do not work as same as character
+	* The character is manage by it's movement component
+	* Actors by it's root component with physics enable
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Setting", meta = (DisplayName = "BumperForce"))
+	float m_bumperActorForceFactor = 100.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Setting", meta = (DisplayName = "ValidClassToBump"))
+	TArray<TSubclassOf<AActor>> m_validClassToBump;
+
+	/**************************** End Settings ******************************/
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Component", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> m_boxComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Component", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USceneComponent> m_root;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<UStaticMeshComponent> m_baseBumper;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<UStaticMeshComponent> m_topBumper;
+
+public:
+
+	/**************************** FUNCTION ******************************/
+public:
+
 	APMBumperBase();
 
 protected:
-	// Called when the game starts or when spawned
+
+	UFUNCTION()
+	virtual void BindBoxComponentEvent();
+
+	UFUNCTION(BlueprintCallable)
+	virtual void OnBoxComponentOverlapped(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/**************************** OVERRIDE ******************************/
+protected:
+
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+
 	virtual void Tick(float DeltaTime) override;
 
 };

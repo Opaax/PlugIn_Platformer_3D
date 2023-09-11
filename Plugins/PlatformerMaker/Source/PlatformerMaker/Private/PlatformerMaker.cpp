9755990e2ec0fd71281c127b7356e09dbf1288c4
Enguerran_COBERT <enguerran.cobert@gmail.com>
2023-09-11 21:07:45 +0200

    [Add] Plugin setting

--- Plugins/PlatformerMaker/Source/PlatformerMaker/Private/PlatformerMaker.cpp
+++ Plugins/PlatformerMaker/Source/PlatformerMaker/Private/PlatformerMaker.cpp
@@ -1,6 +1,8 @@
 // 2023 Copyright Enguerran COBERT, Inc. All Rights Reserved.
 
 #include "PlatformerMaker.h"
+#include "ISettingsModule.h"
+#include "Settings/PlatformerMaker_Settings.h"
 
 #define LOCTEXT_NAMESPACE "FPlatformerMakerModule"
 
@@ -8,13 +10,22 @@ DEFINE_LOG_CATEGORY(LogPlatformerPlugin);
 
 void FPlatformerMakerModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+	{
+		SettingsModule->RegisterSettings("Project", "Plugins", "PlatformerMaker_Settings",
+			LOCTEXT("RuntimeSettingsName", "Platformer Maker"), LOCTEXT("RuntimeSettingsDescription", "You can set here some value to "),
+			GetMutableDefault<UPlatformerMaker_Settings>());
+	}
 }
 
 void FPlatformerMakerModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+	{
+		SettingsModule->UnregisterSettings("Project", "Plugins", "PlatformerMaker_Settings");
+	}
 }
 
 #undef LOCTEXT_NAMESPACE

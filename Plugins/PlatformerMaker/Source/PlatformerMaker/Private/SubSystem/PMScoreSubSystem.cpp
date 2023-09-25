9a5c64b098a15687360400439f7e7b61a7f904cc
Enguerran_COBERT <enguerran.cobert@gmail.com>
2023-09-26 00:32:51 +0200

    [Add] Score system cmd register

--- Plugins/PlatformerMaker/Source/PlatformerMaker/Private/SubSystem/PMScoreSubSystem.cpp
+++ Plugins/PlatformerMaker/Source/PlatformerMaker/Private/SubSystem/PMScoreSubSystem.cpp
@@ -4,6 +4,28 @@
 #include "SubSystem/PMScoreSubSystem.h"
 #include "Settings/PlatformerMaker_Settings.h"
 
+//Unreal
+#if WITH_EDITOR
+#include "HAL/IConsoleManager.h"
+#include "Misc/DefaultValueHelper.h"
+#endif
+
+void UPMScoreSubSystem::AddScore_Implementation(int32 AddScore)
+{
+	m_currentScore += AddScore;
+
+	UE_LOG(LogTemp, Warning, TEXT("Current Score %d"), m_currentScore);
+
+	OnScoreUpdated.Broadcast(m_currentScore);
+}
+
+void UPMScoreSubSystem::RemoveScore_Implementation(int32 RemoveScore)
+{
+	m_currentScore = FMath::Clamp(m_currentScore -= RemoveScore, 0, TNumericLimits<int32>::Max());
+
+	OnScoreUpdated.Broadcast(m_currentScore);
+}
+
 bool UPMScoreSubSystem::ShouldCreateSubsystem(UObject* Outer) const
 {
 	UPlatformerMaker_Settings* lPluginSetting = GetMutableDefault<UPlatformerMaker_Settings>();
@@ -14,3 +36,94 @@ bool UPMScoreSubSystem::ShouldCreateSubsystem(UObject* Outer) const
 
 	return Super::ShouldCreateSubsystem(Outer);
 }
+
+void UPMScoreSubSystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	//Register some cmd
+#if WITH_EDITOR
+	m_cmdSubScore.Add(IConsoleManager::Get().RegisterConsoleCommand(TEXT("PMAddScore"),
+		TEXT("This a command to Add Score"),
+		FConsoleCommandWithArgsDelegate::CreateUObject(this, &UPMScoreSubSystem::DebugAddScore),
+		ECVF_Default));
+
+	m_cmdSubScore.Add(IConsoleManager::Get().RegisterConsoleCommand(TEXT("PMRemoveScore"),
+		TEXT("This a command to Remove Score"),
+		FConsoleCommandWithArgsDelegate::CreateUObject(this, &UPMScoreSubSystem::DebugRemoveScore),
+		ECVF_Default));
+#endif
+}
+
+void UPMScoreSubSystem::Deinitialize()
+{
+	Super::Deinitialize();
+
+	//Unregister some cmd
+#if WITH_EDITOR
+	for (auto& CmdConsole : m_cmdSubScore)
+	{
+		if (CmdConsole != nullptr)
+		{
+			IConsoleManager::Get().UnregisterConsoleObject(CmdConsole);
+		}
+	}
+#endif
+}
+
+//Debug function to be register
+#if WITH_EDITOR
+void UPMScoreSubSystem::DebugRemoveScore(const TArray<FString>& Args) 
+{
+	if (Args.Num() < 1)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can't Remove Score, Not enough arguments"));
+	}
+	else if (Args.Num() > 1)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can't Remove Score, Too much arguments"));
+	}
+
+	if (GetWorld())
+	{
+		if (Args[0].IsNumeric())
+		{
+			int32 lRemove;
+			FDefaultValueHelper::ParseInt(Args[0], lRemove);
+
+			RemoveScore(lRemove);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Can't Remove Score, Can't parse the arguments as int"));
+		}
+	}
+}
+
+void UPMScoreSubSystem::DebugAddScore(const TArray<FString>& Args)
+{
+	if (Args.Num() < 1) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can't Add Score, Not enough arguments"));
+	}
+	else if (Args.Num() > 1)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can't Add Score, Too much arguments"));
+	}
+
+	if (GetWorld()) 
+	{
+		if (Args[0].IsNumeric()) 
+		{
+			int32 lAdded;
+			FDefaultValueHelper::ParseInt(Args[0], lAdded);
+
+			AddScore(lAdded);
+		}
+		else 
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Can't Add Score, Can't parse the arguments as int"));
+		}
+	}
+}
+#endif
--- Plugins/PlatformerMaker/Source/PlatformerMaker/Public/SubSystem/PMScoreSubSystem.h
+++ Plugins/PlatformerMaker/Source/PlatformerMaker/Public/SubSystem/PMScoreSubSystem.h
@@ -6,8 +6,14 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "PMScoreSubSystem.generated.h"
 
+class IConsoleCommande;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FScoreSubsystemSignature, int32, Score);
+
 /**
- * 
+ * Subsystem to handle score.
+ * This system is very simple, you can add feature such as getting score from save.
+ * You can allow the system spawn in Project setting - Plugin - Platformer Maker
  */
 UCLASS()
 class PLATFORMERMAKER_API UPMScoreSubSystem : public UWorldSubsystem
@@ -15,8 +21,40 @@ class PLATFORMERMAKER_API UPMScoreSubSystem : public UWorldSubsystem
 	GENERATED_BODY()
 
 	/**************************** MEMBERS ******************************/
+private:
+	TArray<IConsoleObject*> m_cmdSubScore;
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Score", BlueprintGetter = "GetCurrentScore", meta = (DisplayName = "CurrentScore"))
+	int32 m_currentScore = 0;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Score")
+	FScoreSubsystemSignature OnScoreUpdated;
+
 	/**************************** FUNCTION ******************************/
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable ,Category = "Score")
+	void AddScore(int32 AddScore);
+	virtual void AddScore_Implementation(int32 AddScore);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Score")
+	void RemoveScore(int32 RemoveScore);
+	virtual void RemoveScore_Implementation(int32 RemoveScore);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Score")
+	FORCEINLINE int32 GetCurrentScore() const { return m_currentScore; }
+
 	/**************************** OVERRIDE ******************************/
 public:
 	bool ShouldCreateSubsystem (UObject* Outer) const override;
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	/**************************** Debug ******************************/
+#if WITH_EDITOR
+public:
+	void DebugAddScore(const TArray<FString>& Args);
+	void DebugRemoveScore(const TArray<FString>& Args);
+#endif
 };
